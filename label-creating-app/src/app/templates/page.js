73da89ced1f246a92efee246229059a786dc9e5f
Zelalem Mekonnen <zelliott33@gmail.com>
2024-12-06 16:44:33 -0600

    ready for some css

--- label-creating-app/src/app/templates/page.js
+++ label-creating-app/src/app/templates/page.js
@@ -4,7 +4,7 @@ import TemplateOne from "@/app/components/TemplateOne"
 
 export default function TemplatePage() {
   return (
-    <div className="flex justify-center items-center h-4 bg-gray-100 overflow-auto">
+    <div className="flex justify-center items-center h- bg-gray-100 overflow-auto">
       <TemplateOne />
       <TemplateOne />
       <TemplateOne />
