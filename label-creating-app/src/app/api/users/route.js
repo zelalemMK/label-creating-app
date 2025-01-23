af09d0b6991b93c559d07d41c7917b9ac1cbb183
Zelalem Mekonnen <zelliott33@gmail.com>
2025-01-23 16:39:58 -0600

    changin api/user path

--- label-creating-app/src/app/api/users/route.js
+++ label-creating-app/src/app/api/users/route.js
@@ -1,28 +1,28 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-export default function UsersList() {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    async function fetchUsers() {
-      try {
-        const response = await fetch("/api/users");
-        const data = await response.json();
-        setUsers(data.users);
-      } catch (error) {
-        console.log("Error fetching users", error);
-      }
-    }
-    fetchUsers();
-  }, []);
+// export default function UsersList() {
+//   const [users, setUsers] = useState([]);
+//   useEffect(() => {
+//     async function fetchUsers() {
+//       try {
+//         const response = await fetch("/api/users");
+//         const data = await response.json();
+//         setUsers(data.users);
+//       } catch (error) {
+//         console.log("Error fetching users", error);
+//       }
+//     }
+//     fetchUsers();
+//   }, []);
 
-  return (
-    <div>
-      {users.map((user, index) => (
-        <li>
-          <h1 key={index}>{user.name}</h1>
-          <p>{user.email}</p>
-        </li>
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {users.map((user, index) => (
+//         <li>
+//           <h1 key={index}>{user.name}</h1>
+//           <p>{user.email}</p>
+//         </li>
+//       ))}
+//     </div>
+//   );
+// }
