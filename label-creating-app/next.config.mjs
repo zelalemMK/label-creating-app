00e842d120678d63f6e1d233a0f742b8a0b35a50
Zelalem Mekonnen <zelliott33@gmail.com>
2025-02-03 14:56:03 -0600

    changes so the optimization is turned off

--- label-creating-app/next.config.mjs
+++ label-creating-app/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.optimization.minimize = false; // Disable minification in production
+    }
+    return config;
+  }
+};
 
 export default nextConfig;
+
